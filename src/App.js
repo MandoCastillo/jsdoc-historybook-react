1dfd8a35482f9f7c8b50bcb66aa22b3156896b66
Armando Castillo <31118679+MandoCastillo@users.noreply.github.com>
2021-11-29 12:38:47 -0600

    jsdoc en class y functional components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+// @ts-check
 import './App.css';
+import TacoClass from './components/TacoClass';
+import TacoClassJSDoc from './components/TacoClassJSDoc';
+import TacoFunc from './components/TacoFunc';
+import TacoFuncJSDoc from './components/TacoFuncJSDoc';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <TacoFunc sabor="Tripa" conSalsa />
+        <TacoFuncJSDoc sabor="Chorizo" />
+        <TacoClass sabor="Bisteck" />
+        <TacoClassJSDoc sabor="Champiñon" conSalsa numeroDeTortillas={3} />
       </header>
     </div>
   );
 }
 
 export default App;
+
+// https://gist.github.com/DeruiDENG/074b15de1ebc23ee8d307c14198c1231
+// https://www.youtube.com/watch?v=4Qhgoy-4R3g&t=497s
